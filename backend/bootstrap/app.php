b3de1b25049c7dc8de807366fb2e0c11acc49207
ahaydeid <adi.hadi270@gmail.com>
2026-06-08 23:03:57 +0700

    Redirect unauthenticated guests to /admin/login instead of route(login)

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\EnsureRole::class,
         ]);
+        $middleware->redirectGuestsTo('/admin/login');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
